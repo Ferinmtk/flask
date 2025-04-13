f99cbf4ab67c2c5238d77847be8085d56a84061d
ferinmtk <ferinmtk@gmail.com>
2025-04-13 16:26:46 +0300

    alchemy

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+def __repr__(self):
+    return f'<Todo {self.content}>'
 
 @app.route('/')
 def index():
